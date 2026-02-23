b8f1a9c899765b2030073f4c1e26fd930c08bf8a
Shambhavi Mishra <shampri596@gmail.com>
2026-02-23 14:55:12 +0530

    Update PalindromeCheckerApp.java

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,16 +2,9 @@ public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        // UC1: Welcome Message
-        System.out.println("=======================================");
-        System.out.println("   Welcome to Palindrome Checker App   ");
-        System.out.println("          Version 1.0.0               ");
-        System.out.println("=======================================");
-
-        System.out.println("Program will continue to next use case or exit.");
-        System.out.println();
-
+        // ============================
         // UC2: Hardcoded Palindrome Check
+        // ============================
         String word = "madam"; // hardcoded string
         String reversed = new StringBuilder(word).reverse().toString();
 
@@ -23,5 +16,4 @@ public class PalindromeCheckerApp {
 
     }
 
-}
 }
